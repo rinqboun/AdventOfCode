4965d37e4d18d5ec4331788a1bfb7fce3797f2b2
Adam Checinski <adam.b.checinski@gmail.com>
2023-12-02 14:04:13 +0100

    refactor day 2 part 2

--- src/main/kotlin/year2023/Day2Part2.kt
+++ src/main/kotlin/year2023/Day2Part2.kt
@@ -3,20 +3,31 @@ package year2023
 class Day2Part2 {
     private val colors = listOf("red", "green", "blue")
 
-    fun sumOfIDs() = DAY_TWO_FILE_NAME.lines().map { calculateGameFactor(it) }.toList().sum()
+    fun sumOfIDs() = DAY_TWO_FILE_NAME.lines().map(::calculateGameFactor).toList().sum()
 
     private fun calculateGameFactor(gameLine: String): Int {
-        val gameData = gameLine.substringAfter(":").split(";")
-        val colorToValue = gameData.flatMap { oneGameData ->
-            oneGameData.split(",").map { extractColorToValue(it) }
-        }
-        val maxValuesByColor = colorToValue.groupBy({ it.first }, { it.second }).mapValues { it.value.maxOrNull() ?: 0 }
-        return maxValuesByColor.values.reduce { sum, value -> sum * value }
+        val gameData = extractGameData(gameLine)
+        val colorToValue = gameData.flatMap { extractColorToValues(it) }
+        val maxValuesByColor = calculateMaxValuesByColor(colorToValue)
+        return calculateTotalFactor(maxValuesByColor)
     }
 
+    private fun extractGameData(gameLine: String): List<String> = gameLine.substringAfter(":").split(";")
+
+    private fun extractColorToValues(oneGameData: String): List<Pair<String, Int>> = oneGameData.split(",").map(::extractColorToValue)
+
     private fun extractColorToValue(oneThrow: String): Pair<String, Int> {
-        val color = colors.first { oneThrow.contains(it) }
-        val value =  oneThrow.substringBefore(" $color").trim().toInt()
+        val color = findMatchingColor(oneThrow)
+        val value = extractThrowValue(oneThrow, color)
         return Pair(color, value)
     }
+
+    private fun findMatchingColor(oneThrow: String) = colors.first { oneThrow.contains(it) }
+
+    private fun extractThrowValue(oneThrow: String, color: String) = oneThrow.substringBefore(" $color").trim().toInt()
+
+    private fun calculateMaxValuesByColor(colorToValue: List<Pair<String, Int>>): Map<String, Int> =
+        colorToValue.groupBy({ it.first }, { it.second }).mapValues { it.value.maxOrNull() ?: 0 }
+
+    private fun calculateTotalFactor(maxValuesByColor: Map<String, Int>) = maxValuesByColor.values.reduce { result, value -> result * value }
 }
